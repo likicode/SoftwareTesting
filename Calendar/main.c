7647b2b99e7f425dcee19bdcfc2f3d608e28b95a
likicode <likicode@gmail.com>
2016-03-25 23:37:00 +0800

    remove while loop to faciliate test output

--- Calendar/main.c
+++ Calendar/main.c
@@ -36,47 +36,46 @@ int inputValid(int d, int m, int y){
 
 
 int main(int argc, const char * argv[]){
-    while(1){
-        int month[12]= {31,28,31,30,31,30,31,31,30,31,30,31};
-        int d,m,y,nd,nm,ny,ndays;
-        printf("Please enter the year,month,day\n");
-        scanf("%d,%d,%d",&y,&m,&d);
-        int mark = 0;
-        while(mark ==0){
-            if(inputValid(d, m, y) == 0){
-                printf("Please enter the year,month,day\n");
-                scanf("%d,%d,%d",&y,&m,&d);
-                printf("%d,%d,%d",y,m,d);
-            }else{
-                mark = 1;
-            }
+    int month[12]= {31,28,31,30,31,30,31,31,30,31,30,31};
+    int d,m,y,nd,nm,ny,ndays;
+    printf("Please enter the year,month,day\n");
+    scanf("%d,%d,%d",&y,&m,&d);
+    /*
+    *** for real running, the user will continue input date until he gets right data frame
+    int mark = 0;
+    while(mark ==0){
+        if(inputValid(d, m, y) == 0){
+            printf("Please enter the year,month,day\n");
+            scanf("%d,%d,%d",&y,&m,&d);
+            printf("%d,%d,%d",y,m,d);
+        }else{
+            mark = 1;
         }
-        ndays = month[m-1];
-        if(m == 2 && leapYearFilter(y)){
-            ndays = 29;
-        }
-        nd = d+1;nm=m;ny=y;
-        if (nd > ndays){
-            nd = 1;
-            nm ++;
-        }
-        if (nm >12){
-            nm = 1;
-            ny++;
-        }
-        printf("Given date is: %d,%d,%d\n",y,m,d);
-        printf("next days date is: %d,%d,%d\n",ny,nm,nd);
-        printf("\n");
+    }*/
+    
+    //for test output, I smiplify the while loop to a print sentence
+    if(!inputValid(d, m, y)){
+        printf("invalid date input! Please input again!\n");
+        return 0;
     }
+    
+    ndays = month[m-1];
+    if(m == 2 && leapYearFilter(y)){
+        ndays = 29;
+    }
+    nd = d+1;nm=m;ny=y;
+    if (nd > ndays){
+        nd = 1;
+        nm ++;
+    }
+    if (nm >12){
+        nm = 1;
+        ny++;
+    }
+    printf("Given date is: %d,%d,%d\n",y,m,d);
+    printf("next days date is: %d,%d,%d\n",ny,nm,nd);
+    printf("\n");
     return 0;
 }
 
-/* test cases:
-2008,2,29   ->2008,3,1
-2000,2,28	->2000,2,29
-2001,2,28	->2001,3,1
-1999,2,30	->Please enter the year,month,day
-2006,3,32	->Please enter the year,month,day
-2008,12,31  ->2009,1,1
-*/
 
